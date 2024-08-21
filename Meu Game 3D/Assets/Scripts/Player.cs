9fb08245ac77fb63dee96f262162e3812dbdd0b6
PedroFagundes11 <pedro.fagundes@escolar.ifrn.edu.br>
2024-08-21 10:38:59 -0300

    Pulo e Camera Follow

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int forcaPulo = 7;
+    public bool noChao;
     Rigidbody rb; 
     
     void Start()
@@ -13,12 +18,33 @@ public class Player : MonoBehaviour
         TryGetComponent(out rb);
     }
 
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (!noChao && collision.gameObject.tag == "Chão")
+       {
+            noChao = true;
+       }
+    }
+
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector3 direção = new Vector3(h, 0, v);
-        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+        rb.AddForce(direção * velocidade * Time.deltaTime, ForceMode.Impulse);
+        
+
+        if(Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+
+        }
+
+        if (transform.position.y <= -10)
+        {
+            //jogador caiu
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
